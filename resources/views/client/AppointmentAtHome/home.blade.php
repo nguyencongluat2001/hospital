7f9cf147a514edd2b5732d773e4a6eb8a2327111
luatnc <luatncth2007024@fpt.edu.vn>
2023-08-15 11:07:25 -0700

    f

--- resources/views/client/AppointmentAtHome/home.blade.php
+++ resources/views/client/AppointmentAtHome/home.blade.php
@@ -74,7 +74,7 @@
     }
     .table-responsive.pmd-card.pmd-z-depth{
       height: 100%;
-      max-height: 600px;
+      max-height: 300px;
     }
     #style-1 #table-data thead tr td{
       position: sticky;
