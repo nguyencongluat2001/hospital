40d15da7074c006df0efc27b2ccc5841bc97f612
luatnc <luatncth2007024@fpt.edu.vn>
2023-08-23 07:57:45 -0700

    f

--- modules/Client/Page/AppointmentAtHome/Controllers/AppointmentAtHomeController.php
+++ modules/Client/Page/AppointmentAtHome/Controllers/AppointmentAtHomeController.php
@@ -109,6 +109,7 @@ class AppointmentAtHomeController extends Controller
     public function sendPayment(Request $request)
     {
         $input = $request->all();
+        dd($input);
         $sendPayment =  $this->AppointmentAtHomeService->sendPayment($input);
         return response()->json([
             'status' => $sendPayment
