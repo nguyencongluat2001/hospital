f37544f8efc91aae6943908e33311420fe315f85
Luật Nguyễn <luatnc@efy.com.vn>
2024-07-13 16:24:05 +0700

    f

--- resources/views/dashboard/AppointmentAtHome/loadList.blade.php
+++ resources/views/dashboard/AppointmentAtHome/loadList.blade.php
@@ -32,10 +32,10 @@
                         </td>
                         <td>
                             <span>Ngày tạo: {{ isset($data->created_at) ? $data->created_at : '' }}</span> <br>
+                            <span>Mã code: <span style="font-weight: 600;color:#f90000">{{ isset($data->code) ? $data->code : '' }}</span></span> <br>
                             <span>Tên bệnh nhân: <span style="font-weight: 600;">{{ isset($data->name) ? $data->name : '' }}</span></span> <br>
                             <span>Năm sinh: {{ isset($data->date_birthday) ? $data->date_birthday : '' }}</span> <br>
                             <span>Số điện thoại: {{ isset($data->phone) ? $data->phone : '' }}</span><br>
-                            <span>Mã code: <span style="font-weight: 600;">{{ isset($data->code) ? $data->code : '' }}</span></span> <br>
                             <span>Số tiền:  <span style="font-weight: 600;color:#ff6200">{{ !empty($data->money) ? number_format($data->money,0, '', ',') : '' }}</span> VNĐ </span><br>
                             <span>Lấy mẫu: Lúc <span style="font-weight: 600;">{{ isset($data->hour_sampling) ? $data->hour_sampling : '' }}</span>
                             Ngày <span style="font-weight: 600;">{{Carbon\Carbon::parse($data->date_sampling)->format('d-m-Y')}}</span>
